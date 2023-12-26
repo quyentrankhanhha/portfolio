3445a1390df4834dda7a26fb80df0906baeccfe2
Ha <quyentrankhanhha@gmail.com>
2023-12-26 02:39:46 +0200

    remove badge link

--- app/(app)/contact/page.tsx
+++ app/(app)/contact/page.tsx
@@ -3,32 +3,30 @@ import React from 'react'
 
 export default function ContactPage() {
   return (
-    <section className='h-[55vh]'>
-      <div className='container mx-auto'>
-        <div className='mb-6 grid pt-12 xl:mb-24 xl:h-[480px] xl:grid-cols-2'>
-          <div className='grid xl:justify-items-center '>
-            <div className='mb-4 flex items-center gap-x-4 text-lg text-primary'>
-              <span className='h-[2px] w-[30px] bg-primary'></span>
-              chào, moi, hi
-            </div>
-            <h1 className='h1 max-w xl:max-w-40'>Want to have a chat?</h1>
+    <div className='container mx-auto my-6 xl:mb-24'>
+      <div className='grid h-[500px] pt-12 xl:grid-cols-2'>
+        <div className='grid xl:justify-items-center '>
+          <div className='mb-4 flex items-center gap-x-4 text-lg text-primary'>
+            <span className='h-[2px] w-[30px] bg-primary'></span>
+            chào, moi, hi
           </div>
-          <div className='mt-8 xl:mt-24'>
-            <p>
-              Please contact me directly at{' '}
-              <a className='underline' href='mailto:quyentrankhanhha@gmail.com'>
-                quyentrankhanhha@gmail.com
-              </a>{' '}
-              or{' '}
-              <a className='underline' href='tel:+358417163232'>
-                +358 41 716 3232
-              </a>{' '}
-              or through this form.
-            </p>
-            <ContactForm />
-          </div>
+          <h1 className='h1 max-w xl:max-w-40'>Want to have a chat?</h1>
+        </div>
+        <div className='mt-8 xl:mt-24'>
+          <p>
+            Please contact me directly at{' '}
+            <a className='underline' href='mailto:quyentrankhanhha@gmail.com'>
+              quyentrankhanhha@gmail.com
+            </a>{' '}
+            or{' '}
+            <a className='underline' href='tel:+358417163232'>
+              +358 41 716 3232
+            </a>{' '}
+            or through this form.
+          </p>
+          <ContactForm />
         </div>
       </div>
-    </section>
+    </div>
   )
 }
--- components/Badge.tsx
+++ components/Badge.tsx
@@ -18,32 +18,15 @@ export default function Badge({
   link?: string
 }) {
   return (
-    <div className={`${!link && 'badge' && containerStyles}`}>
-      {link ? (
-        <Link href={link}>
-          <div className={`badge ${containerStyles}`}>
-            <div className='text-3xl text-primary'>{icon}</div>
-            <div className='flex items-center gap-x-2'>
-              <div className='text-4xl font-bold leading-none text-primary'>
-                <CountUp end={endCountNum} delay={1} />
-                {endCountText}
-              </div>
-              <div className='max-w-[70px] text-[15px] font-medium leading-none text-black'>{badgeText}</div>
-            </div>
-          </div>
-        </Link>
-      ) : (
-        <>
-          <div className='text-3xl text-primary'>{icon}</div>
-          <div className='flex items-center gap-x-2'>
-            <div className='text-4xl font-bold leading-none text-primary'>
-              <CountUp end={endCountNum} delay={1} />
-              {endCountText}
-            </div>
-            <div className='max-w-[70px] text-[15px] font-medium leading-none text-black'>{badgeText}</div>
-          </div>
-        </>
-      )}
+    <div className={`badge ${containerStyles}`}>
+      <div className='text-3xl text-primary'>{icon}</div>
+      <div className='flex items-center gap-x-2'>
+        <div className='text-4xl font-bold leading-none text-primary'>
+          <CountUp end={endCountNum} delay={1} />
+          {endCountText}
+        </div>
+        <div className='max-w-[70px] text-[15px] font-medium leading-none text-black'>{badgeText}</div>
+      </div>
     </div>
   )
 }
